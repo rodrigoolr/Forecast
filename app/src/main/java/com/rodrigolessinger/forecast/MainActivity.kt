37bfc4382e3541c2465c61c20b4f9ccec9ce62b1
rodrigoolr <rodrigo_lessinger@hotmail.com>
2016-06-19 16:23:16 -0300

    project start

--- app/src/main/java/com/rodrigolessinger/forecast/MainActivity.kt
+++ app/src/main/java/com/rodrigolessinger/forecast/MainActivity.kt
@@ -2,41 +2,18 @@ package com.rodrigolessinger.forecast
 
 import android.os.Bundle
 import android.support.design.widget.FloatingActionButton
-import android.support.design.widget.Snackbar
 import android.support.v7.app.AppCompatActivity
 import android.support.v7.widget.Toolbar
-import android.view.Menu
-import android.view.MenuItem
 
 class MainActivity : AppCompatActivity() {
+
+    private val toolbar by lazy { findViewById(R.id.toolbar) as Toolbar }
+    private val fab by lazy { findViewById(R.id.fab) as FloatingActionButton }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        val toolbar = findViewById(R.id.toolbar) as Toolbar
         setSupportActionBar(toolbar)
-
-        val fab = findViewById(R.id.fab) as FloatingActionButton
-        fab.setOnClickListener { view -> Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG).setAction("Action", null).show() }
     }
 
-    override fun onCreateOptionsMenu(menu: Menu): Boolean {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        menuInflater.inflate(R.menu.menu_main, menu)
-        return true
-    }
-
-    override fun onOptionsItemSelected(item: MenuItem): Boolean {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        val id = item.itemId
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true
-        }
-
-        return super.onOptionsItemSelected(item)
-    }
 }
